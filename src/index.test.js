4e2b4dcaa5cd9d798552ff5b7ad714fb2100adb1
Juni Brosas <j.brosas@arcanys.com>
2020-04-14 23:38:17 +0800

    write tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,27 @@
 'use strict';
 
 var expect = require('chai').expect;
+var dragonBallNames = require('.');
 
 describe('starwars-names', function () {
-  it('should work', function () {
-    expect(true).to.be.true;
+  it('should have a list of all available names', function () {
+    expect(dragonBallNames.all).to.satisfy(isArrayOfStrings);
+  });
+  it('should allow me to get random name from the list', function () {
+    expect(dragonBallNames.random()).to.satisfy(
+      isIncludedIn(dragonBallNames.all)
+    );
   });
 });
+
+function isArrayOfStrings(array) {
+  return array.every(function (i) {
+    return typeof i === 'string';
+  });
+}
+
+function isIncludedIn(array) {
+  return function (item) {
+    return array.includes(item);
+  };
+}
